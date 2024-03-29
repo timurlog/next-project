41b246986b7b50d4a0f9548a18f3590f791f04bc
timurlog <timur.logie@gmail.com>
2024-03-29 16:52:49 +0100

    added axios and api

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,39 @@
 "use client";
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchContent } from "@/lib/features/content/contentSlice";
 import Navbar from "@/components/Navbar/Navbar";
+import Carousel from "@/components/Carousel/Carousel";
 
 export default function Home() {
   const darkmode = useSelector((state) => state.darkmode.value);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContent());
+  }, [dispatch]);
+
+  const contents = useSelector((state) => state.content.contents);
+  const isLoading = useSelector((state) => state.content.isLoading);
+  const error = useSelector((state) => state.content.error);
+
+  if (isLoading) {
+    return (
+      <div
+        className={`h-screen w-full ${darkmode ? "bg-[#414141]" : "bg-white"}`}
+      >
+        <Navbar />
+        <div className="h-full w-full flex justify-center items-center">
+          <span className="loading loading-spinner loading-lg"></span>
+        </div>
+      </div>
+    );
+  }
+
+  if (error) {
+    return error;
+  }
 
   return (
     <div
@@ -12,6 +42,7 @@ export default function Home() {
       }`}
     >
       <Navbar />
+      <Carousel />
     </div>
   );
 }
--- src/lib/store.js
+++ src/lib/store.js
@@ -3,6 +3,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import darkmodeSlice from "./features/darkmode/darkmodeSlice";
 import showmenuSlice from "./features/showmenu/showmenuSlice";
 import authSlice from "./features/auth/authSlice";
+import contentSlice from "./features/content/contentSlice";
 
 export const makeStore = () => {
   return configureStore({
@@ -10,6 +11,7 @@ export const makeStore = () => {
       darkmode: darkmodeSlice,
       showmenu: showmenuSlice,
       auth: authSlice,
+      content: contentSlice,
     },
   });
 };
