6771ee0cd702ad9935544080505dbb69afeebe9b
timurlog <timur.logie@gmail.com>
2024-04-03 09:14:28 +0200

    added book-list page.jsx

--- src/app/book-list/page.jsx
+++ src/app/book-list/page.jsx
@@ -1,10 +1,40 @@
 "use client";
 import React from "react";
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchContent } from "@/lib/features/content/contentSlice";
 import Navbar from "@/components/Navbar/Navbar";
+import Footer from "@/components/Footer/Footer";
 
 export default function BookList() {
   const darkmode = useSelector((state) => state.darkmode.value);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContent());
+  }, [dispatch]);
+
+  const contents = useSelector((state) => state.content.contents);
+  const isLoading = useSelector((state) => state.content.isLoading);
+  const error = useSelector((state) => state.content.error);
+
+  if (isLoading) {
+    return (
+      <div
+        className={`h-screen w-full ${darkmode ? "bg-[#414141]" : "bg-white"}`}
+      >
+        <Navbar />
+        <div className="h-[80vh] w-full flex justify-center items-center">
+          <span className="loading loading-spinner loading-lg"></span>
+        </div>
+      </div>
+    );
+  }
+
+  if (error) {
+    return error;
+  }
 
   return (
     <div
@@ -13,6 +43,7 @@ export default function BookList() {
       }`}
     >
       <Navbar />
+      <Footer />
     </div>
   );
 }
